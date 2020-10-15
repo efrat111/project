888deb0cc9c3dd2cfc7b00733faab7f718bc844d
efrat111 <72932616+efrat111@users.noreply.github.com>
2020-10-15 19:41:28 +0300

    Add files via upload

---
+++ project1/project1/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();  
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+        private void Button_Click5(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+        private void Button_Click6(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click7(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click8(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click9(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click10(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click11(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click12(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click13(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click14(object sender, RoutedEventArgs e)
+        {
+            //פתיחת חלון חדש ע"י לחיצה על כפתור
+            this.Hide();
+            specific_Categories sc = new specific_Categories();
+            sc.ShowDialog();
+            sc.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            enter_password ep = new enter_password();
+            ep.ShowDialog();
+            this.Close();
+        }
+    }
+}
---
+++ project1/project1/specific_Categories.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project1
+{
+    /// <summary>
+    /// Interaction logic for specific_Categories.xaml
+    /// </summary>
+    public partial class specific_Categories : Window
+    {
+        public specific_Categories()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            specific_Categories sc = new specific_Categories();
+            mw.ShowDialog();
+            this.Close(); 
+        }
+
+        string _prevText = string.Empty;
+        private void search_TextChange(object sender, TextChangedEventArgs e)
+        {
+
+            foreach (var item in search.Items)
+            {
+                if (item.ToString().StartsWith(search.Text))
+                {
+                    _prevText = search.Text;
+                    return;
+                }
+            }
+            search.Text = _prevText;
+        }
+    }
+}
+
+
+
+
+
+
+
+
